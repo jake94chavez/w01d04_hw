88557a75f220c15d472b8919ce76dc6bc0db896d
Jake Chavez <jake94chavez@gmail.com>
2017-12-07 20:29:57 -0600

    another file fix

---
+++ thursday_answers.js
@@ -0,0 +1,11 @@
+/////////////////////////
+/////// Homework 4///////
+/////////////////////////
+
+//Functions
+
+/* 2. A parameter refers to the function setup and the argument refers to when
+the function is called. They are sometimes used interchangably.
+
+3. Return ends the function and stores the value while console.log just logs the
+value to the console. */
