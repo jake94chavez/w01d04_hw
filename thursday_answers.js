c91829dd255238d42aca90acca245a5b4361650c
Jake Chavez <jake94chavez@gmail.com>
2017-12-07 21:40:59 -0600

    Commit 5 - Sum Array

--- thursday_answers.js
+++ thursday_answers.js
@@ -43,3 +43,16 @@ const calculateSide = (sideA, sideB) => {
 
 console.log(calculateSide(8,6));
 console.log(calculateSide(4,3));
+
+// Sum Array
+
+const sumArray = (array) => {
+	let sum = 0;''
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
